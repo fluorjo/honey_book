d6974c1809aee3af604c7bea99251bce4ae66a78
fluorjo <fluorjo@naver.com>
2024-04-12 08:29:23 +0900

    회원가입 화면. 일단 틀만.

---
+++ app/(auth)/create-account/page.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+// import { PASSWORD_MIN_LENGTH } from "@/lib/constants";
+import { useFormState } from "react-dom";
+// import Button from "../../components/button";
+import Input from "../../components/input";
+// import SocialLogin from "../../components/social-login";
+import { createAccount } from "./actions";
+
+export default function CreateAccount() {
+  const [state, dispatch] = useFormState(createAccount, null);
+  return (
+    <div className="flex flex-col gap-10 py-8 px-6">
+      <div className="flex flex-col gap-2 *:font-medium">
+        <h1 className="text-2xl">안녕하세요!</h1>
+        <h2 className="text-xl">Fill in the form below to join!</h2>
+      </div>
+      <form className="flex flex-col gap-3" action={dispatch}>
+        <Input
+          type={"text"}
+          placeholder={"Username"}
+          required={true}
+          name={"username"}
+          errors={state?.fieldErrors.username}
+          minLength={3}
+          maxLength={10}
+        />
+        <Input
+          type={"email"}
+          placeholder={"Email"}
+          required={true}
+          name={"email"}
+          errors={state?.fieldErrors.email}
+        />
+        <Input
+          type={"password"}
+          placeholder={"Password"}
+          required={true}
+          name={"password"}
+          errors={state?.fieldErrors.password}
+          // minLength={PASSWORD_MIN_LENGTH}
+        />
+        <Input
+          type={"password"}
+          placeholder={"Confirm Password"}
+          required={true}
+          name={"confirm_password"}
+          errors={state?.fieldErrors.confirm_password}
+          // minLength={PASSWORD_MIN_LENGTH}
+        />
+        {/* <Button text={"Create account"} /> */}
+      </form>
+      {/* <SocialLogin /> */}
+    </div>
+  );
+}
