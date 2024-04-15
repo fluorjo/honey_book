603c1b16188c5b71ca2d9dda3113206355c3b078
fluorjo <fluorjo@naver.com>
2024-04-15 17:48:41 +0900

    db ㅅ=헌본 ㅜ종

--- lib/db.ts
+++ lib/db.ts
@@ -2,14 +2,14 @@ import { PrismaClient } from "@prisma/client";
 
 const db = new PrismaClient();
 
-async function test() {
-  const token = await db.sMSToken.findUnique({
-    where: {
-      id: 1,
-    },
-  });
-  console.log(token);
-}
-test();
+// async function test() {
+//   const token = await db.sMSToken.findUnique({
+//     where: {
+//       id: 1,
+//     },
+//   });
+//   console.log(token);
+// }
+// test();
 
 export default db;
