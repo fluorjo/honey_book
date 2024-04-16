0cd1e30f35429d14573a6ba9d3aeee06185d5c83
fluorjo <fluorjo@naver.com>
2024-04-16 16:14:37 +0900

    댓글 부분 일단 밑작업만.

--- app/(tabs)/posts/postItem.tsx
+++ app/(tabs)/posts/postItem.tsx
@@ -4,12 +4,23 @@ import {
   ChatBubbleBottomCenterIcon,
   HandThumbUpIcon,
 } from "@heroicons/react/24/outline";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from "react";
-import { PostType } from "./schema";
+import { useForm } from "react-hook-form";
+import { CommentType, PostType, commentSchema } from "./schema";
 interface PostItemProps {
   post: PostType;
 }
+
 const PostItem: React.FC<PostItemProps> = ({ post }) => {
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm<CommentType>({
+    resolver: zodResolver(commentSchema),
+  });
   const [showComments, setShowComments] = useState(true);
 
   const toggleComments = () => {
@@ -41,15 +52,15 @@ const PostItem: React.FC<PostItemProps> = ({ post }) => {
       {showComments && (
         <div>
           <div className="bg-green-300">{post.title}의 댓글 목록 .</div>
-          <div className="bg-blue-500">
+          <form className="bg-blue-500">
             <textarea
               className="bg-blue-200"
               name="description"
-              placeholder="Reply this post"
+              placeholder="Comment this post"
               required
             />
-            <button className="bg-orange-400 w-full">Reply!</button>
-          </div>
+            <button className="bg-orange-400 w-full">Comment!</button>
+          </form>
         </div>
       )}
     </div>
--- app/(tabs)/posts/schema.ts
+++ app/(tabs)/posts/schema.ts
@@ -15,5 +15,16 @@ export const postSchema = z.object({
     likes: z.number(),
   })
 });
+export const commentSchema = z.object({
+  id: z.number(),  // or z.string() depending on your database ID type
+  payload: z.string({
+    required_error: "Description is required",
+  }), 
+  created_at: z.date(),  // Assuming 'created_at' is a Date object
+  _count: z.object({
+    likes: z.number(),
+  })
+});
 
 export type PostType = z.infer<typeof postSchema>;
+export type CommentType = z.infer<typeof commentSchema>;
