8975e6d0b26f19c8a87b9789de4181ff176d6bf1
fluorjo <fluorjo@naver.com>
2024-04-24 17:12:20 +0900

    일단 라우트는 대충 만들어봤는데.

---
+++ app/api/likeStatus/[postId]/route.ts
@@ -0,0 +1,56 @@
+"use server";
+
+import getSession from "@/lib/session";
+import { PrismaClient } from "@prisma/client";
+
+// async function getLikeStatus(postId: number) {
+//   const session = await getSession();
+//   const isLiked = await db.like.findUnique({
+//     where: {
+//       id: {
+//         postId,
+//         userId: session.id!,
+//       },
+//     },
+//   });
+//   const likeCount = await db.like.count({
+//     where: {
+//       postId,
+//     },
+//   });
+//   return {
+//     likeCount,
+//     isLiked: Boolean(isLiked),
+//   };
+// }
+
+export async function GET(
+  _request: Request,
+  { params }: { params: { postId: string } },
+  response: Response
+) {
+  const session = await getSession();
+  const db = new PrismaClient();
+
+  try {
+    const isLiked = await db.like.findUnique({
+      where: {
+        id: {
+          postId: parseInt(params.postId),
+          userId: session.id!,
+        },
+      },
+    });
+    const likeCount = await db.like.count({
+      where: {
+        postId: parseInt(params.postId),
+      },
+    });
+    return {
+      likeCount,
+      isLiked: Boolean(isLiked),
+    };
+  } catch (error) {
+    console.error("Error fetching comments:", error);
+  }
+}
