4e8a61f2746919257b76eb10189c53693918f9b0
fluorjo <fluorjo@naver.com>
2024-04-29 02:59:16 +0900

    profile build error3

--- app/(tabs)/profile/page.tsx
+++ app/(tabs)/profile/page.tsx
@@ -1,22 +1,43 @@
 import db from "@/lib/db";
 import getSession from "@/lib/session";
 import { formatToTime } from "@/lib/utils";
-import ProfileAvatar from "@/app/components/profileAvatar";
+import { notFound } from "next/navigation";
+
 import { logOut } from "@/app/(auth)/login/userAction";
-import { GetServerSideProps } from 'next';
+import ProfileAvatar from "@/app/components/profileAvatar";
+import { unstable_cache } from "next/cache";
 
-interface ProfileProps {
-  user: {
-    username: string;
-    avatar: string;
-    created_at: Date;
-    id: number;
-  };
+async function getUser(id: number) {
+  const session = await getSession();
+  if (session.id) {
+    const user = await db.user.findUnique({
+      where: {
+        id: session.id,
+      },
+    });
+    if (user) {
+      return user;
+    }
+  }
+  notFound();
 }
+const getCachedUser = unstable_cache(
+  async (id) => getUser(id),
+  ["my-app-user"],
+  {
+    tags: ["userStatus"],
+    revalidate: 1,
+  }
+);
 
-export default function Profile({ user }: ProfileProps) {
+
+export default async function Profile({ userID }: any) {
+
+  const user = await getCachedUser(userID);
+
   return (
     <div className="flex flex-col items-center space-y-1">
+
       <ProfileAvatar
         user={{
           userName: user.username,
@@ -25,36 +46,10 @@ export default function Profile({ user }: ProfileProps) {
           id: user.id,
         }}
       />
-      <span>Joined {formatToTime(user.created_at.toString())}</span>
+      <span>Joined {formatToTime(user?.created_at.toString())}</span>
       <form action={logOut}>
         <button className="btn btn-primary mt-1">Log out</button>
       </form>
     </div>
   );
-}
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const userID = parseInt(context.params?.userID as string);
-  const user = await getUser(userID);
-  if (!user) {
-    return {
-      notFound: true,
-    };
-  }
-  return {
-    props: { user },
-  };
-}
-
-async function getUser(id: number) {
-  const session = await getSession();
-  if (session.id && session.id === id) {
-    const user = await db.user.findUnique({
-      where: {
-        id,
-      },
-    });
-    return user;
-  }
-  return null;
 }
