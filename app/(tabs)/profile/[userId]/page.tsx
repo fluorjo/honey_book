b96d35594a7ffc8d37ae3f98ea664f8908977019
fluorjo <fluorjo@naver.com>
2024-04-29 02:56:04 +0900

    profile build error2

---
+++ app/(tabs)/profile/[userId]/page.tsx
@@ -0,0 +1,75 @@
+import db from "@/lib/db";
+import getSession from "@/lib/session";
+import { formatToTime } from "@/lib/utils";
+import { notFound } from "next/navigation";
+
+import { logOut } from "@/app/(auth)/login/userAction";
+import ProfileAvatar from "@/app/components/profileAvatar";
+import { unstable_cache } from "next/cache";
+
+async function getUser(id: number) {
+  const session = await getSession();
+  if (session.id) {
+    const user = await db.user.findUnique({
+      where: {
+        id: session.id,
+      },
+    });
+    if (user) {
+      return user;
+    }
+  }
+  notFound();
+}
+const getCachedUser = unstable_cache(
+  async (id) => getUser(id),
+  ["my-app-user"],
+  {
+    tags: ["userStatus"],
+    revalidate: 1,
+  }
+);
+
+// async function Username() {
+//   try {
+//     const user = await getUser();
+//     return (
+//       <div>
+//         <h1>{user?.username} </h1>
+//         <span>Joined {formatToTime(user?.created_at.toString())}</span>
+//       </div>
+//     );
+//   } catch (error) {
+//     console.error("Failed to fetch user:", error);
+//     return <h1>Error fetching user.</h1>;
+//   }
+// }
+
+export default async function Profile({ userID }:any) {
+  // const logOut = async () => {
+  //   "use server";
+  //   const session = await getSession();
+  //   await session.destroy();
+  //   redirect("/login");
+  // };
+
+  const user = await getCachedUser(userID);
+
+  return (
+    <div className="flex flex-col items-center space-y-1">
+
+      <ProfileAvatar
+        user={{
+          userName: user.username,
+          avatar: user.avatar,
+          created_at: user.created_at,
+          id: user.id,
+        }}
+      />
+      <span>Joined {formatToTime(user?.created_at.toString())}</span>
+      <form action={logOut}>
+        <button className="btn btn-primary mt-1">Log out</button>
+      </form>
+    </div>
+  );
+}
